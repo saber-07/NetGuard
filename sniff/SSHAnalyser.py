ddafa1b6320febf0e5e4adaa3d3d14155fab7d97
jeremynguyen950 <92586996+jeremynguyen950@users.noreply.github.com>
2023-04-01 16:28:37 +0200

    V2

--- sniff/SSHAnalyser.py
+++ sniff/SSHAnalyser.py
@@ -1,92 +1,75 @@
 from scapy.all import *
-import json
+
 SSHconnexions = {}
-SSHattempts = {}
+SSHPasswordAttempts = {}
 SSHsuccess = {}
-SSHlogins = {}
+SSHLoginAttempts = {}
 
 def SSHAnalysis(pkt):       #analyse les paquets ssh
 
     src = pkt[IP].src
     dst = pkt[IP].dst
     key = "dst:%s, src:%s" % (dst,src)
-    port = pkt[TCP].dport
-    warning1 = "Login attempt"
-    warning2 = "Certified IP, connection successful"
-    warning3 = "Potential Brute Force, Malicious IP"
+    dport = pkt[TCP].dport
+   
+    
+    if not key in SSHLoginAttempts:
+        SSHLoginAttempts[key] = {}
 
-    if not key in SSHlogins:
-        SSHlogins[key] = {}
+    if not dst in SSHLoginAttempts[key]:
+        SSHLoginAttempts[key][dst] = 0
 
-    if not port in SSHlogins[key]:
-        SSHlogins[key][port] = 0
-
-        if pkt[IP].len == 87-14 or pkt[IP].len == 99-14:
-            if port in SSHlogins[key]:
-                SSHlogins[key][port] += 1
-
-        print("Connection attempt from: \n IP:%s\n length:%s\n ethernet: %s\n" % (
-            pkt[IP].src, pkt[IP].len + 14, pkt[Ether].src))
-
+    if pkt[IP].len == 73 or pkt[IP].len == 85:
+        SSHLoginAttempts[key][dst] +=1
+        print("%s Connection attempt from: \n IP:%s\n length:%s\n ethernet: %s\n" % (SSHLoginAttempts[key][dst],pkt[IP].src, pkt[IP].len + 14, pkt[Ether].src))
 
     if not key in SSHsuccess:   #paquets correspondant au succès de login
                 SSHsuccess[key] = {}
 
 
-    if not port in SSHsuccess[key]:
-            SSHsuccess[key][port] = 0
+    if not dport in SSHsuccess[key]:
+            SSHsuccess[key][dport] = 0
 
-    if pkt[IP].len == 682-14 or pkt[IP].len == 710 - 14:  #paquets correspondant à la réussite de connexion
-            if port in SSHsuccess[key]:
-                    SSHsuccess[key][port] += 1
+    if pkt[IP].len == 668 or pkt[IP].len == 696:  #paquets correspondant à la réussite de connexion
+            if dport in SSHsuccess[key]:
+                    SSHsuccess[key][dport] += 1
             print("\t\t.....................SSH success: %s\n" % SSHsuccess[key][port])
 
-if not key in SSHattempts:
-        SSHattempts[key] = {}
-    if not port in SSHattempts[key]:
-        SSHattempts[key][port] = 0
+    if not key in SSHPasswordAttempts: #crée le dictionnaire de clés de tentative
+        SSHPasswordAttempts[key] = {}
+    if not dport in SSHPasswordAttempts[key]:
+        SSHPasswordAttempts[key][dport] = 0 #crée le dictionnaire de clés avec son port
+        # paquets correspondant aux aux interactions de login
+    if pkt[IP].len == 116 or  pkt[IP].len == 136 or pkt[IP].len == 128:
+            if port in SSHPasswordAttempts[key]:
+                SSHPasswordAttempts[key][dport] += 1
+            # eventuelles tentative de brute force
 
-    if pkt[IP].len == 130-14 or  pkt[IP].len == 150-14 or pkt[IP].len == 142 - 14:  #paquets correspondant aux aux interactions de login
+    if (SSHPasswordAttempts[key][dport] > 10 or SSHLoginAttempts[key][dst] > 10) and SSHsuccess[key][dport] == 0:  # eventuelle tentative de brute force
+        if pkt[IP].len == 73 or pkt[IP].len == 85:
+            print("Potential brute forcing detected. \n")
 
-            if port in SSHattempts[key]:
-                SSHattempts[key][port] += 1
-
-    if not key in SSHconnexions:
+    if not key in SSHconnexions: #compteur de paquets serveur du même port (connexion établie)
         SSHconnexions[key] = {}
-    if not port in SSHattempts[key]:
-        SSHconnexions[key][port]=0
-    if port in SSHconnexions[key]:       #paquets correspondant à toutes les interactions du serveur ssh
-            SSHconnexions[key][port] += 1
-
-    if SSHattempts[key][port] > 10 and SSHsuccess[key][port] == 0:  # eventuelle tentative de brute force si + de 4 tentatives de connexion ET s'il n'y a pas eu de réussite
-        print("Potential brute forcing detected. \n")
-
-    if pkt[IP].len == 142-14 or pkt[IP].len == 102-14 or pkt[IP].len == 206 - 14: #Déconnexion
-        print("\t\t..........DISCONNECT...........")
-        SSHsuccess[key][port] = 0
+    if not dport in SSHconnexions[key]:
+        SSHconnexions[key][dport]=0
+    if dport in SSHsuccess[key]:
+        SSHconnexions[key][dport]+=1
 
 
-    with open("listeIP.txt", "a") as file:  # dump les IPS dans un JSON
+    if pkt[IP].len == 128 or pkt[IP].len == 88 or pkt[IP].len == 192: #Déconnexion
+        print("\t\t..........DISCONNECT...........\n")
+        SSHsuccess[key][dport] = 0
+        SSHlogin[key][dst] = 0
 
-        if pkt[IP].len == 87 - 14 or 99-14:&
-            json.dump({"src": src, "dst": dst}, file)
-            json.dump({"warning": warning1}, file)
-        elif pkt[IP].len == 682 - 14 or pkt[IP].len == 710 - 14:
-            json.dump({"src": src, "dst": dst}, file)
-            json.dump({"warning": warning2 }, file)
-        elif SSHattempts[key][port] > 10 and SSHsuccess[key][port] == 0:
-            json.dump({"src": src, "dst": dst}, file)
-            json.dump({"warning": warning3}, file)
-        file.write("\n")
 
 
 def analyzePkt(pkt):
 
     if pkt.haslayer(TCP) and pkt[TCP].sport == 22:      #on filtre uniquement sur le port ssh
         SSHAnalysis(pkt)
-        if pkt[IP].len == 682-14 or pkt[IP].len == 710-14:
+        if pkt[IP].len == 668 or pkt[IP].len == 696:
             print(" \t\t............Connection Established.............\n")
 
 
 sniff(filter="tcp", prn=analyzePkt, count=0)
-
