acaba3f81d8b692b3133fb25a45d81a9876e5fce
jeremynguyen950 <92586996+jeremynguyen950@users.noreply.github.com>
2023-04-01 18:23:28 +0200

    V3

--- cli.py
+++ cli.py
@@ -1,20 +1,51 @@
 import os
 import sys
 import argparse
-from sniff import ICMPspoofing, ARPspoofing, ipClassification
+from sniff import SSHAnalyser, ICMPspoofing, ARPspoofing, SmurfAttack, SYNscan, mailCredantials, ipClassification, PortScan, PortScanning
 
 def main():
     if os.geteuid() != 0:
         print('Warning: this script requires root privileges', file=sys.stderr)
-    parser = argparse.ArgumentParser(description='')
+    parser = argparse.ArgumentParser(description='Hello')
 
-    parser.add_argument("sniffer", choices=["sniffer1", "sniffer2", "sniffer3"],
+    parser.add_argument("sniffer", choices=["ssh","icmp","arp","smurf","syn","mail","ip","pscan","pscanning"],
                         help="which sniffer to use")
+
+
     args = parser.parse_args()
 
-    if args.sniffer == "sniffer1":
+
+
+    if args.sniffer == "ssh":
+        SSHAnalyser.sniff()
+
+    elif args.sniffer == "icmp":
         ICMPspoofing.sniff()
-    elif args.sniffer == "sniffer2":
+
+    elif args.sniffer == "arp":
         ARPspoofing.sniff()
-    elif args.sniffer == "sniffer3":
+
+    elif args.sniffer == "smurf":
+        SmurfAttack.sniff()
+
+    elif args.sniffer == "syn":
+        printf("syn")
+        SYNscan.sniff()
+
+    elif args.sniffer == "mail":
+        mailCredantials.sniff()
+
+    elif args.sniffer == "ip":
         ipClassification.sniff()
+
+    elif args.sniffer == "pscan":
+        PortScan.sniff()
+
+    elif args.sniffer == "pscanning":
+        PortScanning.sniff()
+
+
+
+
+if __name__ == '__main__':
+    main()
