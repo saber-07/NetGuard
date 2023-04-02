d3915f4be46dc56199e48240f146b0c13b9a90b2
jeremynguyen950 <92586996+jeremynguyen950@users.noreply.github.com>
2023-04-02 16:46:00 +0200

    Update SSHAnalyser.py

--- sniff/SSHAnalyser.py
+++ sniff/SSHAnalyser.py
@@ -1,4 +1,5 @@
 from scapy.all import *
+import sys
 import json
 SSHconnexions = {}
 SSHPasswordAttempts = {}
@@ -11,66 +12,38 @@ def SSHAnalysis(pkt):       #analyse les paquets ssh
     dst = pkt[IP].dst
     key = "dst:%s, src:%s" % (dst,src)
     dport = pkt[TCP].dport
-    warning1 = "Login attempt"
-    warning2 = "Certified IP, connection successful"
-    warning3 = "Potential Brute Force, Malicious IP"
-    if not key in SSHLoginAttempts:
-        SSHLoginAttempts[key] = {}
+    SSHLoginAttempts.setdefault(key, {})
+    SSHLoginAttempts[key].setdefault(dst, 0)
+    SSHPasswordAttempts.setdefault(key, {})
+    SSHPasswordAttempts[key].setdefault(dport, 0)
+    SSHsuccess.setdefault(key, {})
+    SSHsuccess[key].setdefault(dport, 0)
+    SSHconnexions.setdefault(key, {})
+    SSHconnexions[key].setdefault(dport, 0)
 
-    if not dst in SSHLoginAttempts[key]:
-        SSHLoginAttempts[key][dst] = 0
-
-    if pkt[IP].len == 73 or pkt[IP].len == 85:
+    if pkt[IP].len in [73, 85] and dst in SSHLoginAttempts[key]:
         SSHLoginAttempts[key][dst] +=1
         print("%s Connection attempt from: \n IP:%s\n length:%s\n ethernet: %s\n" % (SSHLoginAttempts[key][dst],pkt[IP].src, pkt[IP].len + 14, pkt[Ether].src))
 
-    if not key in SSHsuccess:   #paquets correspondant au succès de login
-                SSHsuccess[key] = {}
-
-
-    if not dport in SSHsuccess[key]:
-            SSHsuccess[key][dport] = 0
-
-    if pkt[IP].len == 668 or pkt[IP].len == 696:  #paquets correspondant à la réussite de connexion
-            if dport in SSHsuccess[key]:
-                    SSHsuccess[key][dport] += 1
+    if pkt[IP].len in [668,696] and dport in SSHsuccess[key]:  #paquets correspondant à la réussite de connexion
+            SSHsuccess[key][dport] += 1
             print("\t\t.....................SSH success: %s\n" % SSHsuccess[key][dport])
 
-    if not key in SSHPasswordAttempts: #crée le dictionnaire de clés de tentative
-        SSHPasswordAttempts[key] = {}
-    if not dport in SSHPasswordAttempts[key]:
-        SSHPasswordAttempts[key][dport] = 0 #crée le dictionnaire de clés avec son port
-        # paquets correspondant aux aux interactions de login
-    if pkt[IP].len == 116 or  pkt[IP].len == 136 or pkt[IP].len == 128:
-            if port in SSHPasswordAttempts[key]:
-                SSHPasswordAttempts[key][dport] += 1
+    if pkt[IP].len in [116, 128, 136] and dport in SSHPasswordAttempts[key]:
+        SSHPasswordAttempts[key][dport] += 1
             # eventuelles tentative de brute force
 
-    if (SSHPasswordAttempts[key][dport] > 10 or SSHLoginAttempts[key][dst] > 10) and SSHsuccess[key][dport] == 0:  # eventuelle tentative de brute force
-        if pkt[IP].len == 73 or pkt[IP].len == 85:
-            print("Potential brute forcing detected. \n")
+    if (SSHPasswordAttempts[key][dport] > 10 or SSHLoginAttempts[key][dst] > 10) and pkt[IP].len in [73, 85]:  # eventuelle tentative de brute force
+        print("Potential brute forcing detected. \n")
 
-    if not key in SSHconnexions: #compteur de paquets serveur du même port (connexion établie)
-        SSHconnexions[key] = {}
-    if not dport in SSHconnexions[key]:
-        SSHconnexions[key][dport]=0
-    if dport in SSHsuccess[key]:
+    if dport in SSHconnexions[key]:
         SSHconnexions[key][dport]+=1
 
-
-    if pkt[IP].len == 128 or pkt[IP].len == 88 or pkt[IP].len == 192: #Déconnexion
+    if pkt[IP].len in [128,88,192]: #Déconnexion
         print("\t\t..........DISCONNECT...........\n")
         SSHsuccess[key][dport] = 0
         SSHLoginAttempts[key][dst] = 0
 
+sniff(filter="tcp and port 22", prn=SSHAnalysis)
 
 
-def analyzePkt(pkt):
-
-    if pkt.haslayer(TCP) and pkt[TCP].sport == 22:      #on filtre uniquement sur le port ssh
-        SSHAnalysis(pkt)
-        if pkt[IP].len == 668 or pkt[IP].len == 696:
-            print(" \t\t............Connection Established.............\n")
-
-
-sniff(filter="tcp", prn=analyzePkt, count=0)
